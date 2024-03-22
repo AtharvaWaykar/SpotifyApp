3442481385418d137723a07ffa9bf3b146e9162b
Joshua <js>
2024-03-22 18:19:51 -0400

    created our login page, sign in&signup. I watched a tutorial and used firebase as our database - JOSHUA GEORGE

---
+++ app/src/main/java/com/example/spotifyapp/dialog_forgot.java
@@ -0,0 +1,14 @@
+package com.example.spotifyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dialog_forgot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_forgot);
+    }
+}
